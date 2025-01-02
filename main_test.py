d5c410e3d6015ca9a7aac31fa8211f55d42f01e2
Alex <67759297+gnlxpy@users.noreply.github.com>
2025-01-02 22:08:23 +0400

    Update main_test.py
    update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,2 +1,8 @@
 def hello():
     return print('Hello World!')
+
+def say(msg):
+    if len(msg) < 20 and msg is type(string):
+        return print(msg)
+    else:
+        return print(f'message is big!')
